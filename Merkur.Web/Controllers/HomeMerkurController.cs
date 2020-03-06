990e3787d138a617684461ade19ddfd347888f5f
Ronaldo <fzelayab@unah.hn>
2020-03-05 21:14:19 -0600

    Cuerta Entrega Merkur 2.0

---
+++ Merkur.Web/Controllers/HomeMerkurController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Merkur.Web.Controllers
+{
+    public class HomeMerkurController : Controller
+    {
+        // GET: HomeMerkur
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
